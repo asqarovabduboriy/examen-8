65cd7e177034c8b56b1076513d18ca45de764894
Abduboriy <asqarovabduboriy8@gmail.com>
2024-07-04 16:49:23 +0500

    up date

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <>
       <div className="loading_wrapper">
-        <div class="loader"></div>
+        <div className="loader"></div>
       </div>
     </>
   );
